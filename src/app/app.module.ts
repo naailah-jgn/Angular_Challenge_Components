609837e105c54fcdf550600965501b4428038e06
Naailah JUGON <jugonnaailah@gmail.com>
2023-05-12 14:07:11 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BlockComponent } from './block/block.component';
 import { AppComponent } from "./app.component";
+import { FormsModule } from '@angular/forms';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [AppComponent,
-    BlockComponent
+    BlockComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-block",
   templateUrl: "./block.component.html",
   styleUrls: ["./block.component.css"]
 })
-export class BlockComponent {
-  title = "CodeSandbox";
+export class BlockComponent implements OnInit {
+  name: string = "Bob";
+  imageSrc: string = "http://placekitten.com/200/300";
+  constructor() {}
+  ngOnInit(): void {
+  }
+
+  sayHello(): void {
+    alert("Bonjour");
+  }
 }
